0f508d32ec959bfd6b8cfe248d71c693b8ae7ee6
astil <andstil@gmail.com>
2014-10-17 19:11:05 +0200

    Revert "Revert "sendpifmelding""
    This reverts commit a0f2e7a958c84fa4df9ae0d2b7213448b5dd33ec.

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -1,10 +1,24 @@
 /**
  * 
  */
-$(document).ready(function() {
-    $.ajax({
-        url: "http://bouvet-code-camp.azurewebsites.net/api/game/base/hentgjeldendepost/55"
-    }).then(function(data) {
-       $('.navn').append(data.navn);
-    });
+$(document).ready(function () {
+
+	$( "#sendpifmelding" ).submit(function(event) {
+		sendpifmelding(event, this);		
+	});
+    
 });	
+
+function sendpifmelding(event, this) {
+    event.preventDefault();
+	
+	$.post("http://bouvet-code-camp.azurewebsites.net/api/game/base/sendpifmelding",
+    		    {'Type': 'Fritekst','Innhold': 'TEST','LagId': '4c97faa'},
+    		    function(data, textStatus, jqXHR)
+    		    {
+    		        $('.melding').append(textStatus);
+    		    }).fail(function(jqXHR, textStatus, errorThrown) 
+    		    {
+    		        alert(textStatus);
+    		    });
+};
