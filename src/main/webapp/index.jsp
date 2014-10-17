af3d63683b82a3fd014de6fdf340c5424fd17cc5
Jan Olav <jan.kjode@bouvet.no>
2014-10-18 00:43:49 +0200

    Kart i base...

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,28 @@
 <html>
 
 <head>
+    <style type="text/css">
+        html, body,
+        #map-canvas { height: 90%; margin: 0; padding: 0;}
+    </style>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+<script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnAMY08WLnqlrf9xN6VyDwcWDXu6dwvRM">
+</script>
+
 <script type="text/javascript" src="js/base.js"></script>
+
+    <script type="text/javascript">
+        var map;
+    </script>
+
 </head>
 
 <body>
+
+<div id="map-canvas"></div>
 
 <table width="100%" border="1">
     <thead>
--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -3,6 +3,9 @@
  */
 $(document).ready(function () {
 
+    var map = initializeMap();
+    setMyPositionMarker(map);
+
     $.ajax({
         url: "http://bouvet-code-camp.azurewebsites.net/api/game/base/hentgjeldendepost/4c97faa"
     }).then(function(data) {
@@ -45,18 +48,58 @@ $(document).ready(function () {
 		console.log(data);
 		sendpifmelding(event, data);		
 	});
-});	
+});
 
 function sendpifmelding(event, data) {
+    /*
     event.preventDefault();
 	var postData = data;
 	console.log(postData);
 	$.ajax({
 		url : "http://bouvet-code-camp.azurewebsites.net/api/game/base/sendpifmelding",
 		type : 'POST',
-		data : postData
+		data : postData,
 		success: function(data) {
 			alert("success");
 		}
 	  });    
+     */
+};
+
+function initializeMap() {
+
+    var mapOptions = {
+        center: { lat: 60, lng: 9},
+        zoom: 16
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    setCurrentPosition(map);
+    return map;
+}
+
+function setCurrentPosition(map) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        });
+    }
+}
+
+function setMyPositionMarker(map) {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title:"Kor i svartre er eg?"
+            });
+
+
+            marker.setMap(map);
+            map.setCenter(myLocation);
+        });
+    }
+
 };
